d7f1c308ce573fe17a0a477122aba267a954dd10
realonlytomd <mclaughlintomd@gmail.com>
2018-01-17 13:34:21 -0600

    small change to api.routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -9,8 +9,8 @@
 var express = require("express");
 
 var router = express.Router();
-// Requiring our burger model
-var burger = require("../models");
+// Requiring the burger model
+var db = require("../models");
 
 // Routes
 // =============================================================
@@ -18,7 +18,7 @@ module.exports = function(router) {
 
   // GET route for getting all of the burgers
   router.get("/", function(req, res) {
-    burger.findAll({})
+    db.Burger.findAll({})
     .then(function(dbBurger) {
       res.json(dbBurger);
     });
@@ -27,7 +27,7 @@ module.exports = function(router) {
   // POST route for saving a new burger
   router.post("/api/burgers", function(req, res) {
     console.log(req.body);
-    burger.create({
+    db.Burger.create({
       burger_name: req.body.burger_name
     })
     .then(function(result) {
@@ -38,10 +38,11 @@ module.exports = function(router) {
   // PUT route for updating devoured
   router.put("/api/burgers/:id", function(req, res) {
     var condition = "id = " + req.params.id;
-    burger.update({
+    db.Burger.update({
       devoured: req.body.devoured
     })
     .then(condition, function(result) {
       res.json(result);
     });
   });
+};
--- server.js
+++ server.js
@@ -24,8 +24,6 @@ app.set("view engine", "handlebars");
 // Import routes and give the server access to them.
 require("./routes/api-routes.js")(app);
 
-app.use("/", routes);
-
 db.sequelize.sync({ force: true }).then(function() {
     app.listen(PORT, function() {
         console.log("App listening on PORT " + PORT);
